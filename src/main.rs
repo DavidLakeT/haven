551339119b42e44a25b8437b8c5cff63b7193dab
DavidLakeT <djcardonan@eafit.edu.co>
2023-06-04 00:22:03 -0500

    Rocket default reset

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
 #[macro_use] extern crate rocket;
 
-#[get("/echo")]
-fn echo_stream(ws: ws::WebSocket) -> ws::Stream!['static] {
-    let ws = ws.config(ws::Config {
-        max_send_queue: Some(5),
-        ..Default::default()
-    });
-
-    ws::Stream! { ws =>
-        for await message in ws {
-            yield message?;
-        }
-    }
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![echo_stream(ws)])
+    rocket::build().mount("/", routes![index])
 }
