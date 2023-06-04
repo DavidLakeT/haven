00837d774e136647f449abd23953961b158de925
DavidLakeT <djcardonan@eafit.edu.co>
2023-06-03 23:32:45 -0500

    Rocket websockets support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/echo")]
+fn echo_stream(ws: ws::WebSocket) -> ws::Stream!['static] {
+    let ws = ws.config(ws::Config {
+        max_send_queue: Some(5),
+        ..Default::default()
+    });
+
+    ws::Stream! { ws =>
+        for await message in ws {
+            yield message?;
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![echo_stream(ws)])
 }
